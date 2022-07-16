af2fc11947785a176f2c476c3b646fea3b41df83
Ridvan Adakli <ridvanadakli0131@gmail.com>
2022-07-16 23:49:18 +0300

    Initial commit

---
+++ src/day01/Q_1.java
@@ -0,0 +1,32 @@
+package day01;
+
+public class Q_1 {
+    public static void main(String[] args) {
+        //verilen 12345 sayisinin rakamlarini yukaridan asagiya
+        //1
+        //2
+        //3
+        //4
+        //5
+        // seklinde yazin
+
+        int sayi=12345;
+        int birler=sayi%10;
+        sayi/=10;
+
+        int onlar=sayi%10;
+        sayi/=10;
+
+        int yuzler=sayi%10;
+        sayi/=10;
+
+        int binler=sayi%10;
+        sayi/=10;
+
+        int onbinler=sayi%10;
+        sayi/=10;
+
+        System.out.println(onbinler+"\n"+binler+"\n"+yuzler+"\n"+onlar+"\n"+birler);
+
+    }
+}
