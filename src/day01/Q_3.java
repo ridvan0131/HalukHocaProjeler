473fed885dc2b2eb5d50bc48844bf64ff62ae1a8
Ridvan Adakli <ridvanadakli0131@gmail.com>
2022-07-17 17:39:09 +0300

    bak

---
+++ src/day01/Q_3.java
@@ -0,0 +1,10 @@
+package day01;
+
+public class Q_3 {
+    public static void main(String[] args) {
+        // Pazartesi kelimesinin her harfini ayri bir satira gelecek sekilde yazdiriniz
+
+        System.out.println("P\na\nz\na\nr\nt\ne\ns\ni ");
+
+    }
+}
