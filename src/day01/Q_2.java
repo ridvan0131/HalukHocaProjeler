7cb77e24c80b816da85d27c4d02909a8da0cc8e6
Ridvan Adakli <ridvanadakli0131@gmail.com>
2022-07-17 17:30:35 +0300

    HalukHoca_2

--- src/day01/Q_2.java
+++ src/day01/Q_2.java
@@ -3,5 +3,9 @@ package day01;
 public class Q_2 {
     public static void main(String[] args) {
 
+        String ifade="\"Hello \""+", "+"\"World\t\\ "+" /\"";
+
+        System.out.println(ifade);
+
     }
 }
