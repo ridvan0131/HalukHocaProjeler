fca063c2fe5ec9a2eef07a4a866ffccf3f7d7dae
Ridvan Adakli <ridvanadakli0131@gmail.com>
2022-07-27 00:57:33 +0300

    Gun bitti

--- src/day05_StringManipulation/Q04.java
+++ src/day05_StringManipulation/Q04.java
@@ -1,8 +1,19 @@
 package day05_StringManipulation;
 
+import java.util.Scanner;
+
 public class Q04 {
     public static void main(String[] args) {
         // Kullanicidan 4 harfli bir kelime isteyin
         // ve girilen kelimeyi tersten yazdirin
+
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Lutfen 4 harfli bir kelime giriniz: ");
+        String kelime=scanner.next();
+
+        System.out.println("*************1. Yontem****************");
+        System.out.println(kelime.charAt(3)+""+kelime.charAt(2)+""+kelime.charAt(1)+""+kelime.charAt(0));
+        System.out.println("*************2. Yontem****************");
+        System.out.println(kelime.substring(3)+kelime.substring(2,3)+kelime.substring(1,2)+kelime.substring(0,1));
     }
 }
